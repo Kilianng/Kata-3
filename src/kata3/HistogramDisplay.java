3014dad5428e2a0f02d972c2802ad32bb7f217d1
usuario <usuario@10.22.148.65>
2017-10-20 11:52:01 +0100

    HistogramDisplay version 3

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -18,11 +18,11 @@ import org.jfree.ui.ApplicationFrame;
  *
  * @author usuario
  */
-public class HistogramDisplay extends ApplicationFrame{
+public class HistogramDisplay <T> extends ApplicationFrame{
     
-    private final Histogram<String> histogram;
+    private final Histogram<T> histogram;
     
-    public HistogramDisplay(Histogram<String> histogram){
+    public HistogramDisplay(Histogram<T> histogram){
         super("Histograma de emails");
         this.histogram=histogram;
         setContentPane(createPanel());
@@ -47,8 +47,8 @@ public class HistogramDisplay extends ApplicationFrame{
     
     private DefaultCategoryDataset createDataset() {
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-        for (String key : histogram.keySet()) {
-            dataSet.addValue(histogram.get(key), "", key);
+        for (T key : histogram.keySet()) {
+            dataSet.addValue(histogram.get(key), "", (Comparable) key);
         }
         return dataSet;
     }
